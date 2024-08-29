7fcc8266ed306b83e0b5e18fdc6b47d4b5748fda
Emmanuel Medina Jurado <emmanuelmed97@gmail.com>
2024-08-29 15:59:40 -0600

    Clase 1

--- Léxico_1/Program.cs
+++ Léxico_1/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+nusing System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lexico_1
+{
+    public class Program
+    {
+        static void Main(string[] args) 
+        {
+            using (Lecturas L = new Lecturas())
+            
+                Token  token = new();
+            T.setContenido("HOLA");
+            T.setClasificacion(Token.Tipos.Identificador);
+
+            Console.Writeline(true.GetContenido()+" = "+true.GetClasificacion());
+
+            T.setContenido("123");
+            T.setClasificacion(Token.Tipos.Numero);
+
+            Console.Writeline(true.GetContenido()+" = "+true.GetClasificacion());
+        
+        }
+    }
+}
--- Léxico_1/Token.cs
+++ Léxico_1/Token.cs
@@ -4,9 +4,41 @@ using System.Linq;
 using System.Threading.Tasks;
 
 namespace Léxico_1
+
+
 {
+
     public class Token
     {
-        
+        public enum Tipos
+    {
+        Identificador,Numero,Caracter
+    }
+        int clasificacion;
+        string contenido;
+public Token()
+{
+    contenido= "";
+    clasificacion =0;
+}
+    }
+
+   public  void setContenido(string contenido)
+    {
+        this.contenido= contenido;
+    }
+    public void setClasificacion(Tipos clasificacion)
+    {
+        this.clasificacion = clasificacion;
+
+    }
+
+    public string GetContenido()
+    {
+        return this.contenido;
+    }
+    public Tipos GetClasificacion()
+    {
+        return this.clasificacion;
     }
 }
